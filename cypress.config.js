ea9fb8b4cbb0606dac2819afa6014ce26f0ee71f
Pâmela Paesi Bieger <53712205+PamelaPaesi@users.noreply.github.com>
2022-08-04 17:58:39 -0300

    ajuste dos dados

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  "pluginsFile": false,
+  /*"pluginsFile": false,
   "viewportHeight": 880,
-  "viewportWidth": 1280,
+  "viewportWidth": 1280,*/
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
