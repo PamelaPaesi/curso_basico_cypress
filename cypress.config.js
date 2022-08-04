f28953eb58e73f90230ab0166f507a0d2a0e069f
Pâmela Paesi Bieger <53712205+PamelaPaesi@users.noreply.github.com>
2022-08-04 18:06:24 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   "viewportHeight": 880,
   "viewportWidth": 1280,*/
   e2e: {
+    baseUrl: './src/index.html',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
