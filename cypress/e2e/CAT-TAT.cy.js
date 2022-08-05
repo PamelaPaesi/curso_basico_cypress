96655eea320ee5be43605512483b845c0be99eb9
Pâmela Paesi Bieger <53712205+PamelaPaesi@users.noreply.github.com>
2022-08-04 21:36:18 -0300

    aula 2 concluida

---
+++ cypress/e2e/CAT-TAT.cy.js
@@ -0,0 +1,81 @@
+describe('Central de Atendimento ao Cliente TAT', function() {
+  it('verifica o título da aplicação', function() {
+      cy.visit('./src/index.html')
+      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+  it('preenche os campos obrigatórios e envia o formulário', function(){
+    cy.visit('./src/index.html')
+      const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
+      cy.get('input#firstName').type('Pâmela')
+      cy.get('#lastName').type('Paesi')
+      cy.get('#email').type('pamelapaesi25@gmail.com')
+      cy.get('#phone').type('51 999452903')
+      cy.get('#product').select('Blog')
+      cy.get('#support-type > :nth-child(3)').click()
+      cy.get('#email-checkbox').click()
+      cy.get('#open-text-area').type(longText,{delay: 0})
+      cy.get('.button').click()
+      cy.contains('Mensagem enviada com sucesso.').should('be.visible')
+  })
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+    cy.visit('./src/index.html')
+      cy.get('#firstName').type('Pâmela')
+      cy.get('#lastName').type('Paesi')
+      cy.get('#email').type('pamelapaesi25@')
+      cy.get('#open-text-area').type('Atendimento excelente')
+      cy.get('.button').click()
+      cy.get('.error').should('be.visible')
+  })
+  it('campo telefone só aceita numeros', function(){
+      cy.visit('./src/index.html')
+      cy.get('#phone')
+      .type('abcdefghij')
+      .should('have.value', '')
+  })
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+      cy.visit('./src/index.html')
+      cy.get('#firstName').type('Pâmela')
+      cy.get('#lastName').type('Paesi')
+      cy.get('#email').type('pamelapaesi25@gmail.com')
+      cy.get('#product').select('Blog')
+      cy.get('#support-type > :nth-child(3)').click()
+      cy.get('#phone-checkbox').click()
+      cy.get('#open-text-area').type('Atendimento excelente')
+      cy.get('.button').click()
+      cy.get('.error').should('be.visible')
+  })
+
+  it('preenche e limpa ps campos nome, sobrenome, email e telefone', function(){
+      cy.visit('./src/index.html')
+      cy.get('#firstName').type('Pâmela').should('have.value', 'Pâmela')
+      .clear().should('have.value', '')
+      cy.get('#lastName').type('Paesi').should('have.value', 'Paesi')
+      .clear().should('have.value', '')
+      cy.get('#email').type('pamelapaesi25@gmail.com').should('have.value', 'pamelapaesi25@gmail.com')
+      .clear().should('have.value', '')
+      cy.get('#phone').type('51999452903').should('have.value', '51999452903')
+      .clear().should('have.value', '')
+  })
+
+  it('exibe mensahem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+      cy.visit('./src/index.html')
+      cy.get('.button').click()
+      cy.get('.error').should('be.visible')
+  })
+
+  it('envia o dormulário com sucesso usando um comando customizado', function(){
+      cy.visit('./src/index.html')
+      cy.fillMandatoryFieldsAndSubmit()
+      cy.contains('Mensagem enviada com sucesso.').should('be.visible')
+  })
+
+  it('usando contains para apertar em um botão', function(){
+      cy.visit('./src/index.html')
+      cy.get('#firstName').type('Pâmela')
+      cy.get('#lastName').type('Paesi')
+      cy.get('#email').type('pamelapaesi25@gmail.com')
+      cy.get('#open-text-area').type('Atendimento excelente')
+      cy.contains('button', 'Enviar').click()
+      cy.get('.success').should('be.visible')
+  })
+})
