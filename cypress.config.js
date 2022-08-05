6547a69446c5f1ba7b0b918cd3157fafbfc8d710
Pâmela Paesi Bieger <53712205+PamelaPaesi@users.noreply.github.com>
2022-08-04 21:40:32 -0300

    aula 2 concluida

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  /*"pluginsFile": false,
-  "viewportHeight": 880,
-  "viewportWidth": 1280,*/
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
